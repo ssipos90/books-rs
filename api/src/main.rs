53dbea1c22a76d3e0a2aa5fbf096f44b6fc378c6
Sebastian Sipos <sebastiansipos90@gmail.com>
2021-09-17 13:08:50 +0300

    WIP

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,14 +1,15 @@
 extern crate dotenv;
 
 use dotenv::dotenv;
+use crate::models::{Book,InsertBook};
 use ormx::Insert;
 use rocket::form::{Form, FromForm};
-use rocket::response::status::NotFound;
-use rocket::{Build, Error, Rocket, data::FromData, response::content::Json};
-use rocket::serde::{Deserialize};
+use rocket::http::Status;
+use rocket::response::Result;
+use rocket::response::content::Json;
+use rocket::response::status::{self};
 use sqlx::postgres::PgPool;
 extern crate rocket;
-
 mod models;
 
 //mod models;
@@ -18,28 +19,39 @@ mod models;
 
 // }
 
+#[derive(FromForm)]
 struct CreateBook {
-    author_id: u32;
+    author_id: i32,
+    title: String,
+    genre: models::Genre
 }
 
-impl From<InsertBook> for FromFormStruct {
-
+impl From<models::InsertBook> for CreateBook {
+    fn from(model: models::InsertBook) -> Self {
+        Self {
+            author_id: model.author_id,
+            title: model.title,
+            genre: model.genre,
+        }
+    }
 }
 
 #[rocket::post("/book", data = "<input>")]
-async fn world(pool: &rocket::State<PgPool>, input: Form<models::InsertBook>) -> Result<Json<models::Book>, Error> {
-    match pool.acquire().await {
-        Ok(db) => {
-            let book = models::InsertBook {
-                author_id: input.author_id,
-                genre: input.genre,
-                title: input.title
-            }.insert(&mut *db).await;
+async fn createBook(pool: &rocket::State<PgPool>, input: Form<CreateBook>) -> Result {
+    let db = pool.acquire()
+        .await
+        .map_err(|e| status::Custom(Status::InternalServerError, "Error acquiring pool"))?;
 
-            Ok(book)
-        },
-        Err(e) => Err(NotFound)
+    let book = InsertBook {
+        author_id: input.author_id,
+        genre: input.genre,
+        title: input.title
     }
+        .insert(&mut *db)
+        .await
+        .map_err(|e| status::Custom(Status::InternalServerError, "Error inserting"))?;
+
+    Ok(Json(book))
 }
 
 #[rocket::launch]
@@ -51,5 +63,5 @@ async fn rocket() -> _ {
 
     rocket::build()
         .manage::<PgPool>(pool)
-        .mount("/", rocket::routes![world])
+        .mount("/", rocket::routes![createBook])
 }
